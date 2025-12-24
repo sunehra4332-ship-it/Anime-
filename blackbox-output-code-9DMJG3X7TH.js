27fcd9c3cb2077b45efe03c99c7a0edf4a3c04ae
sunehra4332-ship-it <sunehra4332@gmail.com>
2025-12-25 00:04:10 +0600

    Add files via upload

---
+++ blackbox-output-code-9DMJG3X7TH.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Thank you for your message! We\'ll get back to you soon.');
+    // In a real app, send data to a server here
+});
